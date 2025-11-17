b006e94c40792c54a56b39c631229a9df123e69e
DIILEESHA <dileeshanawarathna18@gmail.com>
2025-11-17 23:36:27 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./Home";
 import RsvpForm from "./pages/rsvps/RsvpForm";
 import AdminDashboard from "./pages/rsvps/AdminDashboard";
 import "./App.css";
 import Nav from "./components/nav/Nav";
 
+const AppWrapper = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+};
+
 const App = () => {
+  const location = useLocation();
+
+  // Show Nav only on Home page
+  const showNav = location.pathname === "/";
+
   return (
     <div className="sold">
-        <Nav />
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/rsvp" element={<RsvpForm />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-          </Routes>
-        </Router>
+      {showNav && <Nav />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rsvp" element={<RsvpForm />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
     </div>
   );
 };
 
-export default App;
+export default AppWrapper;
